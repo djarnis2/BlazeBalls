fe9ed65c90d0707024c21e3705c5c795b53dd147
Lars Kristiansen <larkr23@student.sdu.dk>
2025-05-05 20:15:28 +0200

    Added enemy-player collision in the PlayerPostProcessingSystem

---
+++ Player/src/main/java/dk/sdu/cbse/playersystem/PlayerPostProcessingSystem.java
@@ -0,0 +1,29 @@
+package dk.sdu.cbse.playersystem;
+
+import dk.sdu.cbse.common.bullet.Bullet;
+import dk.sdu.cbse.common.bullet.BulletSPI;
+import dk.sdu.cbse.common.data.Entity;
+import dk.sdu.cbse.common.data.GameData;
+import dk.sdu.cbse.common.data.World;
+import dk.sdu.cbse.common.services.IPostEntityProcessingService;
+
+public class PlayerPostProcessingSystem implements IPostEntityProcessingService {
+    @Override
+    public void process(GameData gameData, World world) {
+        for (Entity entity : world.getEntities(Player.class)) {
+            Entity damager = (Entity) entity.getData("collidesWith");
+            if (damager instanceof Bullet) {
+                Player player = (Player) entity;
+                world.removeEntity(damager);
+                player.damage();
+                if (player.getLifePoints() > 0) {
+                    System.out.println(player.getLifePoints());
+                }
+                else {
+                    world.removeEntity(player);
+                    System.out.println("YOU DIED");
+                }
+            }
+        }
+    }
+}
--- Player/src/main/java/module-info.java
+++ Player/src/main/java/module-info.java
@@ -4,5 +4,6 @@ module Player {
     requires CommonBullet;
     provides dk.sdu.cbse.common.services.IGamePluginService with dk.sdu.cbse.playersystem.PlayerPlugin;
     provides dk.sdu.cbse.common.services.IEntityProcessingService with dk.sdu.cbse.playersystem.PlayerControlSystem;
+    provides dk.sdu.cbse.common.services.IPostEntityProcessingService with dk.sdu.cbse.playersystem.PlayerPostProcessingSystem;
 
 }
